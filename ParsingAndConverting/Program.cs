32d787f73463e68ffaed3484452a33588201ca47
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2023-11-25 21:07:28 +0300

    3 task completed

---
+++ ParsingAndConverting/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ParsingAndConverting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            byte b = Convert.ToByte(a);
+            Console.WriteLine(b);
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
