f599b2d2b96956b7b6ee711c09eef181e290eb1d
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2023-11-25 21:34:56 +0300

    4 task finally completed

--- LastTask/Program.cs
+++ LastTask/Program.cs
@@ -6,11 +6,8 @@
         {
             Console.WriteLine("Введите 3 числа через клавишу Enter");
             int a = Int32.Parse(Console.ReadLine());
-            bool b1 = Convert.ToBoolean(a);
             int b = Int32.Parse(Console.ReadLine());
-            bool b2 = Convert.ToBoolean(b);
             int c = Int32.Parse(Console.ReadLine());
-            bool b3 = Convert.ToBoolean(c);
             Console.WriteLine($"Среднее арифметическое данных чисел: {(a + b + c)/3}");
         }
     }
