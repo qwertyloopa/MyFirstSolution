4b406353c2b7d2ae925cad169395a13798a5791d
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2023-11-25 20:43:51 +0300

    Добавьте файлы проекта.

---
+++ MyFirstConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyFirstConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
